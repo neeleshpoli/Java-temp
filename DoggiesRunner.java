98b98de7c658a2e4138a92e679f011b21b6688b0
we sell insurance <72574589+neeleshpoli@users.noreply.github.com>
2023-10-31 09:41:08 -0500

    Update DoggiesRunner.java

--- DoggiesRunner.java
+++ DoggiesRunner.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class DoggiesRunner
+{
+    public static void main( String[] args )
+    {
+       
+        int size;
+       
+        Scanner keyboard = new Scanner(System.in);
+       
+        System.out.print("How many Dogs are in the pack? :: "  );
+        size = keyboard.nextInt();
+       
+        Doggies pack = new Doggies(size);
+       
+       
+        System.out.println("pack :: " + pack);
+        System.out.println("NAME OF OLDEST :: " + pack.getNameOfOldest());
+        System.out.println("NAME OF YOUNGEST :: "+pack.getNameOfYoungest());
+       
 
+       
+    }
+}
