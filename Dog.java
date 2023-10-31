b7c7aa648c96efd4719baeb92d3813445c00f94b
we sell insurance <72574589+neeleshpoli@users.noreply.github.com>
2023-10-31 09:38:46 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,1 +1,22 @@
-
+public class Dog
+{
+    private int age;
+    private String name;
+    public Dog(int a, String n)
+    {
+        age = a;
+        name = n;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public String toString()
+    {
+        return "" + age + " " + name;
+    }
+}
